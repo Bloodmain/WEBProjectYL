1fb8631638c15ba6c7c813ab699cd9d36d616416
Dubinin Dmitrii <dmitriid204@ya.ru>
2021-03-26 01:17:51 +0500

    Изменнено папка загрузки файлов

--- WebProjectYL/user_manager/models.py
+++ WebProjectYL/user_manager/models.py
@@ -4,12 +4,20 @@ import datetime as dt
 # Create your models here.
 
 
+def avatars_directory(instance, filename):
+    return f"user_{instance.user.id}/avatar/{filename}"
+
+
+def news_files_directory(instance, filename):
+    return f"user_{instance.user.id}_news_{instance.id}/files/{filename}"
+
+
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, unique=True, related_name='profile')
     name = models.CharField(max_length=50, verbose_name="Имя", null=True, blank=False)
     surname = models.CharField(max_length=75, verbose_name="Фамилия", null=True, blank=False)
     bio = models.TextField(max_length=500, verbose_name="Описание", default="", null=True, blank=False)
-    avatar = models.ImageField(verbose_name="Фото", null=True)
+    avatar = models.ImageField(verbose_name="Фото", null=True, upload_to=avatars_directory)
     status = models.CharField(max_length=75, default="", verbose_name="Статус", null=True, blank=False)
     birth_date = models.DateField(verbose_name='Дата рождения', null=True, blank=False)
 
@@ -32,7 +40,7 @@ class News(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='news')
     text_content = models.TextField(max_length=1000, verbose_name='Контент', blank=False)
     likes = models.IntegerField(default=0, verbose_name='Лайки')
-    files = models.FileField(null=True, blank=True)
+    files = models.FileField(null=True, blank=True, upload_to=news_files_directory)
 
     def __str__(self):
         return self.text_content[:20] + "..."

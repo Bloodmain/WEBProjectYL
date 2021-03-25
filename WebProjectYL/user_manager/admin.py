90be1c5111762f325ca17037cb516fdd17fa56e7
Dubinin Dmitrii <dmitriid204@ya.ru>
2021-03-26 00:30:04 +0500

    Исправления и доработки

--- WebProjectYL/user_manager/admin.py
+++ WebProjectYL/user_manager/admin.py
@@ -1,7 +1,36 @@
 from django.contrib import admin
-from .models import Profile, News, Сommentary
+from .models import Profile, News, Commentary
+
+
+class AdminProfile(admin.ModelAdmin):
+    list_display = ['user', 'name', 'surname', 'birth_date']
+    list_filter = ['name', 'surname', 'birth_date']
+    list_display_links = ['user', 'name', 'surname', 'birth_date']
+    search_fields = ['user', 'name', 'surname', 'birth_date']
+
+    class Meta:
+        model = Profile
+
+
+class AdminNews(admin.ModelAdmin):
+    list_display = ['user', 'likes']
+    search_fields = ['user', 'text_content']
+    list_filter = ['user', 'likes', 'text_content']
+
+    class Meta:
+        model = News
+
+
+class AdminCommentary(admin.ModelAdmin):
+    list_display = ['user', 'news']
+    search_fields = ['user', 'news', 'text']
+    list_filter = ['user', 'news']
+
+    class Meta:
+        model = Commentary
+
 
 # Register your models here.
-admin.site.register(Profile)
-admin.site.register(News)
-admin.site.register(Сommentary)
+admin.site.register(Profile, AdminProfile)
+admin.site.register(News, AdminNews)
+admin.site.register(Commentary, AdminCommentary)
--- WebProjectYL/user_manager/models.py
+++ WebProjectYL/user_manager/models.py
@@ -5,11 +5,11 @@ import datetime as dt
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=True, related_name='profile')
     name = models.CharField(max_length=50, verbose_name="Имя", null=True, blank=False)
     surname = models.CharField(max_length=75, verbose_name="Фамилия", null=True, blank=False)
     bio = models.TextField(max_length=500, verbose_name="Описание", default="", null=True, blank=False)
-    avatar = models.ImageField(verbose_name="Фото", null=True, blank=False)
+    avatar = models.ImageField(verbose_name="Фото", null=True)
     status = models.CharField(max_length=75, default="", verbose_name="Статус", null=True, blank=False)
     birth_date = models.DateField(verbose_name='Дата рождения', null=True, blank=False)
 
@@ -29,7 +29,7 @@ class Profile(models.Model):
 
 
 class News(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='news')
     text_content = models.TextField(max_length=1000, verbose_name='Контент', blank=False)
     likes = models.IntegerField(default=0, verbose_name='Лайки')
     files = models.FileField(null=True, blank=True)
@@ -42,9 +42,9 @@ class News(models.Model):
         verbose_name_plural = "Новости"
 
 
-class Сommentary(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    news = models.ForeignKey(News, on_delete=models.CASCADE)
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment')
+    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='comment')
     text = models.TextField(max_length=100, null=False, blank=False, default="")
 
     def __str__(self):

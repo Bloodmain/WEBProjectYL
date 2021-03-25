c468ffae1ac05ef4088d2c9ddcba7120d47692dc
Bloodmain <krytoipohtalon@mail.ru>
2021-03-26 02:39:17 +0500

    Добавлена аватарка на главной странице

--- WebProjectYL/user_manager/models.py
+++ WebProjectYL/user_manager/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import datetime as dt
+
+
 # Create your models here.
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, verbose_name="Имя", null=True, blank=False)
     surname = models.CharField(max_length=75, verbose_name="Фамилия", null=True, blank=False)
-    bio = models.TextField(max_length=500, verbose_name="Описание", default="", null=True, blank=False)
-    avatar = models.ImageField(verbose_name="Фото", null=True, blank=False)
-    status = models.CharField(max_length=75, default="", verbose_name="Статус", null=True, blank=False)
+    bio = models.TextField(max_length=500, verbose_name="Описание", default="", null=True,
+                           blank=False)
+    avatar = models.ImageField(upload_to="", verbose_name="Фото", null=True, blank=False)
+    status = models.CharField(max_length=75, default="", verbose_name="Статус", null=True,
+                              blank=False)
     birth_date = models.DateField(verbose_name='Дата рождения', null=True, blank=False)
 
     def __str__(self):
@@ -53,4 +59,3 @@ class Сommentary(models.Model):
     class Meta:
         verbose_name = "Комментарий"
         verbose_name_plural = "Коммментарии"
-
---
+++ WebProjectYL/user_manager/static/js/script.js
@@ -0,0 +1,4 @@
+$('.burger, .menu_btn').click(function () {
+    $('.burger').toggleClass('open');
+    $('.menu').toggleClass('menu_active');
+});

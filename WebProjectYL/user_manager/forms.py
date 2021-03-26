6cb6dcf1ac59c68c79d6e6c4b40822b0521644a9
Bloodmain <krytoipohtalon@mail.ru>
2021-03-26 22:13:04 +0500

    Добавлены формы регистрации и логина, небольшие изменения у двигающихся элементов

--- WebProjectYL/user_manager/forms.py
+++ WebProjectYL/user_manager/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from .models import Profile
+import datetime as dt
 
 from django import forms
 
@@ -10,15 +11,14 @@ class UserLoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
 
     username = forms.CharField(widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': '', 'id': 'username'}))
+        attrs={'class': 'form-control', 'placeholder': '', 'id': 'username'}), required=True)
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={
-
             'class': 'form-control',
             'placeholder': '',
             'id': 'password',
         }
-    ), label='Пароль')
+    ), label='Пароль', required=True)
 
 
 class UserForm(forms.ModelForm):
@@ -26,8 +26,49 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'email', 'password', 'first_name', 'last_name')
 
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': '', 'id': 'username'}),
+        label='Имя пользователя', required=True)
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': '',
+            'id': 'password',
+        }
+    ), label='Пароль', required=True)
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'placeholder': '', 'id': 'email'}),
+        label='Адрес электронной почты', required=True)
+    first_name = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': '', 'id': 'first-name'}),
+        label='Имя', required=True)
+    last_name = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': '', 'id': 'last-name'}),
+        label='Фамилия', required=True)
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('bio', 'status', 'birth_date')
+        fields = ('bio', 'avatar', 'status', 'birth_date')
+
+    birth_date = forms.DateField(widget=forms.SelectDateWidget(
+        attrs={
+            'class': 'form-control',
+            'placeholder': '',
+            'id': 'birthdate',
+        },
+        years=list(range(1950, dt.date.today().year + 1))
+    ), label='Дата рождения', required=True)
+
+    status = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': '', 'id': 'status'}),
+        label='Статус', required=False)
+
+    bio = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'form-control', 'placeholder': '', 'id': 'bio'}),
+        label='О себе', required=False)
+
+    avatar = forms.ImageField(widget=forms.FileInput(
+        attrs={'class': 'form-control', 'placeholder': '', 'id': 'avatar'}),
+        label='Аватар', required=False)
--- WebProjectYL/user_manager/views.py
+++ WebProjectYL/user_manager/views.py
@@ -1,7 +1,6 @@
 from django.contrib import messages
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout
 from .forms import UserLoginForm, UserForm, ProfileForm
-from .models import Profile
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -36,23 +35,23 @@ class Logout(View):
 def register(request):
     if request.method == 'POST':
         user_form = UserForm(request.POST)
-        profile_form = ProfileForm(request.POST)
+        profile_form = ProfileForm(request.POST, request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save(commit=False)
             user.set_password(user_form.cleaned_data['password'])
             user.save()
-
-            user.profile.name = profile_form.cleaned_data['name']
-            user.profile.surname = profile_form.cleaned_data['surname']
+            user.profile.name = user_form.cleaned_data['first_name']
+            user.profile.surname = user_form.cleaned_data['last_name']
             user.profile.bio = profile_form.cleaned_data['bio']
             user.profile.status = profile_form.cleaned_data['status']
+            user.profile.avatar = profile_form.cleaned_data['avatar']
             user.profile.birth_date = profile_form.cleaned_data['birth_date']
             user.profile.save()
 
             messages.success(request, 'Успешная регистрация!')
             return redirect('/login/')
         else:
-            messages.error(request, 'Пожалуйста, исправьте ошибки.')
+            messages.error(request, f'Пожалуйста, исправьте ошибки.')
     else:
         user_form = UserForm()
         profile_form = ProfileForm()

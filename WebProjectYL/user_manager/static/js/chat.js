d19bc661e36997469935c77a731c76f2442ccd03
Bloodmain <krytoipohtalon@mail.ru>
2021-04-23 14:18:36 +0500

    Исправлена ошибка с неопределением юзера в чате

---
+++ WebProjectYL/user_manager/static/js/chat.js
@@ -0,0 +1,57 @@
+const chatID = JSON.parse(document.getElementById('chat-id').textContent);
+const user = JSON.parse(document.getElementById('user').textContent);
+
+const chatSocket = new WebSocket(
+    'ws://'
+    + window.location.host
+    + '/ws/chat/'
+    + chatID
+    + '/'
+);
+
+var first_message = true;
+
+chatSocket.onmessage = function (e) {
+    const data = JSON.parse(e.data);
+    if (first_message) {
+        first_message = false;
+        for (var k in data.messages) {
+            var message = data.messages[k];
+            var tp = 'other-message'
+            if (message[1] === user) {
+                tp = 'our-message'
+            }
+            console.log(message)
+            $('.table-chat tr:last').after('<tr><td class="' + tp + '"><div class="date-send">' + message[2] + '</div><br><div class="message">' + message[0] + '</div></td></tr>');
+        }
+    } else {
+        var tp = 'other-message'
+        console.log(data)
+        if (data.uid === user) {
+            tp = 'our-message'
+        }
+        $('.table-chat tr:last').after('<tr><td class="' + tp + '"><div class="date-send">' + data.date + '</div><br><div class="message">' + data.message + '</div></td></tr>');
+    }
+    var div = $(".dialog");
+    div.scrollTop(div.prop('scrollHeight'));
+};
+
+chatSocket.onclose = function (e) {
+    console.error('Chat socket closed unexpectedly');
+};
+
+
+$('.message-input').focus();
+$('.message-input').keyup(function (e) {
+    if (e.keyCode === 13) {
+        $('.send-message').click();
+    }
+})
+
+$('.send-message').click(function () {
+    const inputDOM = $('.message-input')
+    chatSocket.send(JSON.stringify({
+        'message': inputDOM.val()
+    }));
+    inputDOM.val('');
+})
